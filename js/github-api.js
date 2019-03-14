21cb40322eeec87b0401ae1d77d9bc41d550c52e
Dario Coletto Eggon <d.coletto@eggonemail.com>
2019-03-14 22:39:23 +0100

    moved all js to github-api.js

--- js/github-api.js
+++ js/github-api.js
@@ -35,3 +35,62 @@ class Owner {
         this.login = login;
     }
 }
+
+let initialized = false;
+const github = new GithubApi();
+
+function onInit(){
+    if(initialized) return;
+    initialized = true;
+
+    const cardColumn = document.getElementById("card-column");
+
+    github.onRepoDownloaded = (repo) => {
+        const secondaryCard = document.createElement("div");
+        secondaryCard.id = "card-" + repo.repo_id;
+        secondaryCard.classList.add("base-card");
+        secondaryCard.classList.add("secondary-card");
+
+        const author = document.createElement("p");
+        const authorLink = document.createElement("a");
+        author.classList.add("secondary-card-author");
+        authorLink.href = repo.owner.html_url;
+        authorLink.target = "_blank";
+        authorLink.rel = "noopener noreferrer";
+        authorLink.innerText = "@" + repo.owner.login;
+        author.appendChild(authorLink);
+        secondaryCard.appendChild(author);
+
+        const title = document.createElement("div");
+        const titleSpan = document.createElement("span");
+        titleSpan.innerText = repo.name;
+        title.classList.add("secondary-card-title");
+        title.appendChild(titleSpan);
+        secondaryCard.appendChild(title);
+
+        const desc = document.createElement("p");
+        desc.innerText = repo.description;
+        desc.classList.add("secondary-card-desc");
+        secondaryCard.appendChild(desc);
+
+        const lang = document.createElement("p");
+        lang.innerText = "{ "+repo.lang+" }";
+        lang.classList.add("secondary-card-lang");
+        secondaryCard.appendChild(lang);
+
+        secondaryCard.onclick = (event) => {
+            const tagName = event.target.tagName.toLowerCase();
+            if(tagName !== "a") {
+                window.open(repo.html_url);
+            }
+        };
+
+        cardColumn.appendChild(secondaryCard);
+    };
+    github.initGithub();
+}
+
+// Calling the script twice (here and on the body load) since it is deferred
+// and it's not called in the onload (probably?)
+// Maybe some browser doesn't support deferred, so let's do this
+onInit();
